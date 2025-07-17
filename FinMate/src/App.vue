32846f831eb5606a495aa9507c9e43cd9d3e36ff
Hyunsoo KIM <howeve18@gmail.com>
2025-07-17 11:06:12 +0900

    🎉 Initial Commit

---
+++ FinMate/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import { useRoute } from 'vue-router';
+
+export default {
+  name: 'App',
+  components: {},
+  setup() {
+    const currentRoute = useRoute();
+    return { currentRoute };
+  },
+};
+</script>
