ad741abb1cc006e3dc9e6fa87b37c5b2585d53be
yeni023 <yeeun3011@naver.com>
2025-07-21 16:27:44 +0900

    ✨ 마이페이지 레이아웃 초기 구성 및 사이드바/탑바 적용

---
+++ FinMate/src/views/info/MyInfoView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Sidebar from "@/components/info/Sidebar.vue";
+import TopNavigationBar from "@/components/allshared/TopNavigationBar.vue";
+</script>
+
+<template>
+  <TopNavigationBar />
+  <div class="mypage-container">
+    <Sidebar />
+    <!-- 여기 나중에 UserInfoForm 들어갈 부분 -->
+  </div>
+</template>
