fc22282c04551c35ce06db111e642d5ea66fde94
yeni023 <yeeun3011@naver.com>
2025-07-21 17:38:48 +0900

    🎨 css 공통 속성 관련 기존 코드 추가 및 변경

--- FinMate/src/components/allshared/TopNavigationBar.vue
+++ FinMate/src/components/allshared/TopNavigationBar.vue
@@ -34,8 +34,8 @@
 </template>
 
 <script setup>
-import '../../assets/fonts/font.css';
-import 'hover.css/css/hover-min.css';
+import "@/styles/fonts.css";
+import "hover.css/css/hover-min.css";
 </script>
 
 <style scoped>
@@ -47,7 +47,7 @@ import 'hover.css/css/hover-min.css';
   height: 13vh;
   background-color: #89caa2;
   justify-content: space-between;
-  font-family: 'TMONBlack';
+  font-family: "TMONBlack";
 }
 
 .top-navigation-bar-logo-container {
