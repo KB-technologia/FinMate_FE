1c3e8b42d36c03d20d4283f8f275dbe2afe453eb
Youngjae so <terranbang@gmail.com>
2025-07-25 15:33:12 +0900

    ✨ TrueButton 컴포넌트 구현

---
+++ FinMate/src/components/dailyquiz/TrueButton.vue
@@ -0,0 +1,22 @@
+<script setup>
+const emit = defineEmits(['submitAnswer']);
+
+function TrueClick() {
+  emit('submitAnswer', true);
+}
+</script>
+<template>
+  <button class="TrueButton" @click="TrueClick">O</button>
+</template>
+<style scoped>
+.TrueButton {
+  background-color: rgba(173, 216, 230, 0.6);
+  border: none;
+  border-radius: 8px;
+  padding: 0.8rem 1.5rem;
+  font-size: 1.5rem;
+  cursor: pointer;
+  width: 5vw;
+  height: 10vh;
+}
+</style>
