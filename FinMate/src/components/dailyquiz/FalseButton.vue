f30f209f09f6065d232d9faa57719412b0ac9a71
Youngjae so <terranbang@gmail.com>
2025-07-25 15:31:50 +0900

    ✨ FalseButton 컴포넌트 구현

---
+++ FinMate/src/components/dailyquiz/FalseButton.vue
@@ -0,0 +1,22 @@
+<script setup>
+const emit = defineEmits(['submitAnswer']);
+
+function falseClick() {
+  emit('submitAnswer', false);
+}
+</script>
+<template>
+  <button class="FalseButton" @click="falseClick">X</button>
+</template>
+<style scoped>
+.FalseButton {
+  background-color: rgba(231, 36, 36, 0.21);
+  border: none;
+  border-radius: 8px;
+  padding: 0.8rem 1.5rem;
+  font-size: 1.5rem;
+  cursor: pointer;
+  width: 5vw;
+  height: 10vh;
+}
+</style>
