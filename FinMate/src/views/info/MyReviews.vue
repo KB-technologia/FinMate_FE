c76f0cb73c21754e8607602814db4f081b5d38df
yeni023 <yeeun3011@naver.com>
2025-07-28 17:06:07 +0900

    ✨ 마이페이지 리뷰 정렬 드롭다운 추가

--- FinMate/src/views/info/MyReviews.vue
+++ FinMate/src/views/info/MyReviews.vue
@@ -19,6 +19,17 @@
             </div>
           </div>
         </div>
+        <div class="review-sort-bar">
+          <div class="select-wrapper">
+            <select v-model="selectedSort">
+              <option value="all">전체보기</option>
+              <option value="latest">최신순</option>
+              <option value="high">높은 별점순</option>
+              <option value="low">낮은 별점순</option>
+            </select>
+            <ChevronDown class="select-icon" />
+          </div>
+        </div>
       </RightPanel>
     </div>
   </div>
@@ -26,10 +37,13 @@
 
 <script setup>
 import { ref } from "vue";
+import { ChevronDown } from "lucide-vue-next";
+
 import Sidebar from "@/components/info/Sidebar.vue";
 import RightPanel from "@/components/info/RightPanel.vue";
 import TopNavigationBar from "@/components/allshared/TopNavigationBar.vue";
 
+const selectedSort = ref("all");
 const selectedCategory = ref("all");
 
 const categories = [
@@ -70,14 +84,14 @@ const categories = [
 
 .category-tab-bar-container {
   width: 100%;
-  max-width: 750px;
+  max-width: 47rem;
   border: none;
 }
 
 .category-tab-bar {
   display: flex;
   width: 100%;
-  border-radius: 4px;
+  border-radius: 0.25rem;
   overflow: hidden;
 }
 
@@ -89,12 +103,51 @@ const categories = [
   color: var(--color-white);
   border: none;
   transition: background-color 0.2s ease;
-  border-right: 1px solid var(--color-white);
+  border-right: 0.0625rem solid var(--color-white);
 }
 
 .category-tab-bar button.active {
   background-color: var(--color-primary-yellow);
   color: var(--color-black);
   font-weight: var(--font-weight-bold);
+}
+
+.review-sort-bar {
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+  padding-right: 0.5rem;
+}
+
+.select-wrapper {
+  position: relative;
+  display: inline-block;
+}
+
+.select-wrapper select {
+  padding: 0.5rem 2.5rem 0.5rem 1rem;
+  font-size: 0.9rem;
+  border-radius: 2.5rem;
+  border: 0.0625rem solid var(--color-dark-gray);
+  color: var(--color-black);
+  background-color: var(--color-white);
+  appearance: none;
+  text-align: center;
+
+  outline: none;
+  box-shadow: none;
+}
+
+.select-icon {
+  position: absolute;
+  right: 1rem;
+  top: 50%;
+  transform: translateY(-50%);
+  pointer-events: none;
+  width: 1rem;
+  height: 1rem;
+  color: var(--color-dark-gray);
 }
 </style>
