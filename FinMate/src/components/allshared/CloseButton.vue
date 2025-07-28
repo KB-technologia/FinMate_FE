a3b0b401bb5d3e1ba95df687a48986c3bddca092
Youngjae so <terranbang@gmail.com>
2025-07-28 16:07:38 +0900

    ♻️  CloseButton 컴포넌트 분리

---
+++ FinMate/src/components/allshared/CloseButton.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { X } from 'lucide-vue-next';
+</script>
+<template>
+  <button class="close-button"><X stroke="red" /></button>
+</template>
+<style scoped>
+.close-button {
+  all: unset;
+  cursor: pointer;
+}
+</style>
