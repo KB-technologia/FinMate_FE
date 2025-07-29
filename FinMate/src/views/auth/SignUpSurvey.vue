f2335b27095841c4a90cea31f5554150ea1e0a2c
yeainlee <lyy923923@gmail.com>
2025-07-29 14:34:25 +0900

    ✨ 회원가입 설문 페이지 TopNav, Footer 추가

--- FinMate/src/views/auth/SignUpSurvey.vue
+++ FinMate/src/views/auth/SignUpSurvey.vue
@@ -1,12 +1,46 @@
-<script setup></script>
+<script setup>
+import TopNavigationBar from '@/components/allshared/TopNavigationBar.vue';
+import FooterComponent from '@/components/allshared/FooterComponent.vue';
+// import SurveyComponent from '@/components/survey/SurveyComponent.vue';
+</script>
 
 <template>
-  <div style="background: yellow; padding: 2rem">
-    ✅ 회원가입 설문 페이지입니다!
+  <div class="survey-view">
+    <TopNavigationBar />
+    <div class="survey-view-content">
+      <div class="survey">
+        <!-- <SurveyComponent/> -->
+      </div>
+      <FooterComponent />
+    </div>
   </div>
 </template>
 
 <style scoped>
-.SignUpSurveu {
+.survey-view {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+}
+
+.survey-view-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow-x: hidden;
+  overflow-y: auto;
+  width: 100vw;
+  height: 100vh;
+}
+
+.survey {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
 }
 </style>
