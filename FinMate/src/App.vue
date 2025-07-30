f8e923de93a51b37c99afa8c9ffe240f56edfb48
yeni023 <yeeun3011@naver.com>
2025-07-30 10:07:05 +0900

    ♻️ App.vue 리팩토링: 토큰 만료 검사 및 toast 초기화 로직 개선
    - authStore의 initialize() 호출로 유효성 판단
    - 토스트 메시지 추가 및 router.replace("/login") 처리

--- FinMate/src/App.vue
+++ FinMate/src/App.vue
@@ -7,16 +7,29 @@
 
 <script setup>
 import { ref, onMounted } from "vue";
-import { useRoute } from "vue-router";
-import ToastContainer from "@/components/allshared/ToastContainer.vue";
-import { setToastInstance } from "@/composables/toastManager";
+import { useRouter } from "vue-router";
 
-const currentRoute = useRoute();
+import { setToast, setToastInstance } from "@/composables/toastManager";
+import ToastContainer from "@/components/allshared/ToastContainer.vue";
+import { useAuthStore } from "@/api/auth/authStore";
+
+const router = useRouter();
 const toastRef = ref(null);
 
 onMounted(() => {
   if (toastRef.value) {
     setToastInstance(toastRef.value);
+  }
+
+  const authStore = useAuthStore();
+  const isValid = authStore.initialize();
+
+  if (!isValid) {
+    setToast(
+      "세션이 만료되어 로그아웃되었습니다. 다시 로그인해주세요",
+      "warning"
+    );
+    router.replace("/login");
   }
 });
 </script>
