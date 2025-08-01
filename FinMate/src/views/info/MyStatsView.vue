21f98616fa98f31d4269d470951b0240adecdd94
Hyunsoo KIM <howeve18@gmail.com>
2025-08-01 16:10:16 +0900

    ✨ Mystat 페이지 TopNavBar 정렬 및 Footer 추가

--- FinMate/src/views/info/MyStatsView.vue
+++ FinMate/src/views/info/MyStatsView.vue
@@ -1,21 +1,46 @@
 <script setup>
-import Sidebar from "@/components/info/Sidebar.vue";
-import TopNavigationBar from "@/components/allshared/TopNavigationBar.vue";
-import RightPanel from "@/components/info/RightPanel.vue";
-import UserStatsPanel from "@/components/info/UserStatsPanel.vue";
+import Sidebar from '@/components/info/Sidebar.vue';
+import TopNavigationBar from '@/components/allshared/TopNavigationBar.vue';
+import RightPanel from '@/components/info/RightPanel.vue';
+import UserStatsPanel from '@/components/info/UserStatsPanel.vue';
+import FooterComponent from '../../components/allshared/FooterComponent.vue';
 </script>
 
 <template>
-  <TopNavigationBar />
-  <div class="mypage-container">
-    <Sidebar />
-    <RightPanel :scroll="true" class="stats-right-panel">
-      <UserStatsPanel />
-    </RightPanel>
+  <div class="mypage">
+    <TopNavigationBar />
+    <div class="scroll-able">
+      <div class="mypage-container">
+        <Sidebar />
+        <RightPanel :scroll="true" class="stats-right-panel">
+          <UserStatsPanel />
+        </RightPanel>
+      </div>
+      <FooterComponent />
+    </div>
   </div>
 </template>
 
 <style scoped>
+.mypage {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+}
+.scroll-able {
+  padding-top: 1vh;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow-y: auto;
+  overflow-x: hidden;
+  gap: 2vh;
+}
 .mypage-container {
   display: flex;
   gap: 2rem;
