e5c13937c0d4c80ea963166ad56710e0decea889
yeni023 <yeeun3011@naver.com>
2025-08-13 14:39:26 +0900

    ✨ 퀴즈 모달 intro 구현

---
+++ FinMate/src/components/info/quiz/QuizDailyLimitModal.vue
@@ -0,0 +1,32 @@
+<template>
+  <TalkModalShell :bg-image="quizBg" @close="$emit('close')">
+    <template #bubble>
+      <ModalMascot :src="kiwiIntroQuiz" />
+      <SpeechBubble sender="FINMATE">
+        <TypewriterText
+          :lines="['하루에 딱! 한 번만 도전할 수 있어요. 준비되셨나요?']"
+        />
+        <template #overlay>
+          <FloatingChoiceButtons
+            primaryLabel="시작하기"
+            secondaryLabel="나가기"
+            @primary="$emit('start')"
+            @secondary="$emit('close')"
+          />
+        </template>
+      </SpeechBubble>
+    </template>
+  </TalkModalShell>
+</template>
+
+<script setup>
+import TalkModalShell from "@/components/allshared/talk-modal/TalkModalShell.vue";
+import ModalMascot from "@/components/allshared/talk-modal/ModalMascot.vue";
+import SpeechBubble from "@/components/allshared/talk-modal/SpeechBubble.vue";
+import TypewriterText from "@/components/allshared/talk-modal/TypewriterText.vue";
+import FloatingChoiceButtons from "@/components/allshared/talk-modal/FloatingChoiceButtons.vue";
+import quizBg from "@/assets/images/backgroundImage/quiz-bg.png";
+import kiwiIntroQuiz from "@/assets/images/logos/kiwi-intro-quiz.png";
+
+defineEmits(["close", "start"]);
+</script>
---
+++ FinMate/src/components/info/quiz/QuizRewardIntroModal.vue
@@ -0,0 +1,29 @@
+<template>
+  <TalkModalShell :bg-image="quizBg" @close="$emit('close')">
+    <template #bubble>
+      <ModalMascot :src="kiwiIntroQuiz" />
+      <SpeechBubble sender="FINMATE">
+        <TypewriterText :lines="['오늘의 퀴즈를 풀면 경험치를 드려요!']" />
+        <template #overlay>
+          <FloatingChoiceButtons
+            primaryLabel="다음으로"
+            secondaryLabel="나가기"
+            @primary="$emit('next')"
+            @secondary="$emit('close')"
+          />
+        </template>
+      </SpeechBubble>
+    </template>
+  </TalkModalShell>
+</template>
+
+<script setup>
+import TalkModalShell from "@/components/allshared/talk-modal/TalkModalShell.vue";
+import ModalMascot from "@/components/allshared/talk-modal/ModalMascot.vue";
+import SpeechBubble from "@/components/allshared/talk-modal/SpeechBubble.vue";
+import TypewriterText from "@/components/allshared/talk-modal/TypewriterText.vue";
+import FloatingChoiceButtons from "@/components/allshared/talk-modal/FloatingChoiceButtons.vue";
+import quizBg from "@/assets/images/backgroundImage/quiz-bg.png";
+import kiwiIntroQuiz from "@/assets/images/logos/kiwi-intro-quiz.png";
+defineEmits(["next", "close"]);
+</script>
